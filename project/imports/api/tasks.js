508dafce63e9e081771ea5e362034936f74f0644
titovanton <mail@titovanton.com>
2016-04-04 19:31:34 +0000

    10. Filtering data with publish and subscribe

--- project/imports/api/tasks.js
+++ project/imports/api/tasks.js
@@ -16,6 +16,19 @@ function checkId(id) {
   }
 }
 
+if (Meteor.isServer) {
+  // This code only runs on the server
+  // Only publish tasks that are public or belong to the current user
+  Meteor.publish('tasks', function tasksPublication() {
+    return Tasks.find({
+      $or: [
+        { private: { $ne: true } },
+        { owner: this.userId },
+      ],
+    });
+  });
+}
+
 Meteor.methods({
   'tasks.insert'(text) {
     check(text, String);
@@ -32,15 +45,43 @@ Meteor.methods({
       username: Meteor.user().username,
     });
   },
+
   'tasks.remove'(taskId) {
     checkId(taskId);
 
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error('not-authorized');
+    }
+
     Tasks.remove(taskId);
   },
+
   'tasks.setChecked'(taskId, setChecked) {
     checkId(taskId);
     check(setChecked, Boolean);
 
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error('not-authorized');
+    }
+
     Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
+
+  'tasks.setPrivate'(taskId, setToPrivate) {
+    check(taskId, String);
+    check(setToPrivate, Boolean);
+
+    const task = Tasks.findOne(taskId);
+
+    // Make sure only the task owner can make a task private
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
   },
 });
